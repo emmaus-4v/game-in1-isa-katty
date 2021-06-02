4fc743cd339086e5ced873d2ae685f1ffac713bc
emmauskatty <113344@emmauscollege.nl>
2021-06-02 16:01:00 +0000

    voeg leven game over toe

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const UITLEG = 0;
 const SPELEN = 1;
 const GAMEOVER = 2;
 const WIN = 3;
-var spelStatus = SPELEN;
+var spelStatus = UITLEG;
 
 var kogelX = 0;    // x-positie van kogel
 var kogelY = 0;    // y-positie van kogel
@@ -35,6 +35,7 @@ var vijandX = 0;   // x-positie van vijand
 var vijandY = 0;   // y-positie van vijand
 
 var score = 0; // aantal behaalde punten
+var leven = 3; // aantal leven
 
 const space = 32; // toetsenbord space 
 const leftArrow = 37; // toetsenbord left 
@@ -43,6 +44,7 @@ const upArrow = 38; // toetsenbord up
 const enter = 13; // enter toets up 
 
 var scoreElem; // score element op het scherm
+var levenElem; // leven element op het scherm
 
 var speler; // speler
 
@@ -73,7 +75,7 @@ var spelerImg;
 var tekenVeld = function () {
     // background('blue');
     // Achtergrond plaatje
-    image(bgImg, 0, 0);
+    image(bgImg, 0, 0, width, height);
 
     // teken grond
     image(groundImg, 0, 450, 1240, 200);
@@ -193,7 +195,10 @@ var checkSpelerGeraakt = function () {
  * Zoekt uit of het spel is afgelopen
  * @returns {boolean} true als het spel is afgelopen
  */
-var checkGameOver = function () {
+function checkGameOver() {
+    if (leven <= 0) {
+        return true;
+    }
     return false;
 };
 
@@ -216,6 +221,10 @@ function muntGeraak(speler, geraakteMunt) {
 function piekGeraak() {
     // verminder score
     score -= 1
+
+    // verminder leven
+    leven -= 1
+
     // zet speler op begin locatie
     resetSpeler()
 }
@@ -230,40 +239,38 @@ function resetSpeler() {
 }
 
 /**
- * Zoekt uit of speler gescroord heeft
+ * Zoekt uit of speler gescroord heeft of dood ging
  */
-var checkGescore = function () {
+var checkScoreEnLeven = function () {
     // score aanpassen als speler een munt aanraak
     speler.overlap(munten, muntGeraak);
 
     // speler is bij doel
-    speler.overlap(doel, () => {
-        score += 5
-        // als speler doel heeft bereik creeer munten opnieuw
-        creeerMunten()
-        resetSpeler()
-    });
+    speler.overlap(doel, spelerBijDoel);
 
     // speler is gevalen tussen de obstakels
     speler.overlap(pieken, piekGeraak);
 
-    // update score
+    // update score en elven
     scoreElem.html('Score: ' + score)
+    levenElem.html('Leven: ' + leven)
 };
+
+/**
+ * speler is bij doel
+ */
+function spelerBijDoel() {
+    score += 5
+    // als speler doel heeft bereik creeer munten opnieuw
+    creeerMunten()
+    resetSpeler()
+}
 
 /**
  * Zoekt uit of speler gewonnen heeft
  * @returns {boolean} true als speler heeft gewonnen
  */
 var checkWin = function () {
-    return false;
-};
-
-/**
- * Zoekt uit of het spel is afgelopen
- * @returns {boolean} true als het spel is afgelopen
- */
-var checkGameOver = function () {
     return false;
 };
 
@@ -294,15 +301,23 @@ function setup() {
     grond = createSprite(650, 649, 1240, 200)
     // grond.shapeColor = "green"
     grond.visible = false
+
     // toon score
     scoreElem = createP('Score:').position(750, 0)
+        .style('color: black').style('font-size', '24px')
+        .style('font-weight: bold')
+
+    // toon leven
+    levenElem = createP('Leven:').position(550, 0)
         .style('color: black').style('font-size', '24px')
         .style('font-weight: bold')
 
     // creeer speler 
     speler = createSprite(210, 480, 90, 90);
     speler.addImage(spelerImg)
-    // speler.shapeColor = "white"
+
+    // voeg variable jumping toe aan speler om te bepalen of de speler kan springen
+    // dit zorg ervoor dat de speler maar 1 keer kan springen
     speler.jumping = false
 
     // creeer munten
@@ -331,6 +346,25 @@ function kanSpringen() {
     }
 }
 
+/**
+ * teken getint achtergrond
+ */
+function tekenGetintAchtergrond() {
+    // achtergrond plaatje
+    image(bgImg, 0, 0, width, height);
+    // transparant tint
+    fill(0, 0, 0, 100);
+    rect(0, 0, width, height);
+}
+
+/**
+ *  verberg score en leven
+ */
+function verBergScoreEnLeven() {
+    scoreElem.style('visibility: hidden;');
+    levenElem.style('visibility: hidden;');
+}
+
 
 /**
  * draw
@@ -340,12 +374,15 @@ function kanSpringen() {
 function draw() {
     switch (spelStatus) {
         case UITLEG:
-
+            tekenGetintAchtergrond()
             // uitleg teksten
             textSize(30)
+            fill(255, 255, 255, 255);
             text('Gebruik pijltjes toets om te bewegen', 420, 300, 500, 500)
             text('Spatie of pijltje omhoog om te springen', 400, 400, 700, 500)
             text('Klik enter om te starten', 500, 500, 500, 500)
+
+            verBergScoreEnLeven();
 
             // als speler op enter klik, start het spel
             if (keyIsDown(enter)) {
@@ -354,8 +391,13 @@ function draw() {
 
             break;
         case SPELEN:
+
             // teken veld
             tekenVeld();
+
+            // toon score en leven
+            scoreElem.style('visibility: visible;')
+            levenElem.style('visibility: visible;')
 
             // zwartekracht
             speler.velocity.y += GRAVITY
@@ -380,11 +422,8 @@ function draw() {
             // beweging controles voor speler
             beweegSpeler();
 
-            // check of speler iets gescore heeft
-            checkGescore()
-
-            // teken spelers, obstakel, grond en munten
-            drawSprites();
+            // check score en leven
+            checkScoreEnLeven()
 
             if (checkWin()) {
                 spelStatus = WIN;
@@ -392,6 +431,32 @@ function draw() {
             if (checkGameOver()) {
                 spelStatus = GAMEOVER;
             }
+
+            // teken spelers, obstakel, grond en munten
+            drawSprites();
+
+            break;
+        case GAMEOVER:
+            // Achtergrond plaatje
+            tekenGetintAchtergrond();
+
+            // uitleg teksten
+            textSize(30)
+            fill(255, 255, 255, 255);
+            textAlign(CENTER);
+            text('GAME OVER!', 420, 300, 500, 500)
+            text('Klik enter om naar uitlegscherm te gaan', 420, 500, 500, 500)
+
+            verBergScoreEnLeven()
+
+            // reset score en leven en ga terug naar uitlegscherm
+            if (keyIsDown(enter)) {
+                spelStatus = UITLEG
+                leven = 3
+                score = 0
+            }
+
             break;
     }
 }
+
